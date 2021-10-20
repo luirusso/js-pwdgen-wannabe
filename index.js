e9f7f4c582496dc5ad020ff9160f26da51f1b0a4
luirusso <luirusso_@hotmail.it>
2021-10-20 15:26:09 +0200

    project setup and added first 3 prompts for name, surname and favorite color

---
+++ index.js
@@ -0,0 +1,14 @@
+// Ottieni nome dell'utente
+
+const nomeUtente = prompt("Qual è il tuo nome?");
+console.log(nomeUtente)
+
+// Ottieni cognome dell'utente
+
+const cognomeUtente = prompt("Qual è il tuo cognome?");
+console.log(cognomeUtente)
+
+// Ottieni colore preferito dell'utente
+const colorePref = prompt("Qual è il tuo colore preferito?");
+console.log(colorePref)
+
